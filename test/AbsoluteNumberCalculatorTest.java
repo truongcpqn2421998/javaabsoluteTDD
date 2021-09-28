5c2e784aa8ff8960d3c84737e2f5407e29b90176
truongcpqn2421998 <truongcpqn24219982@gmail.com>
2021-09-28 11:22:54 +0700

    update

--- test/AbsoluteNumberCalculatorTest.java
+++ test/AbsoluteNumberCalculatorTest.java
@@ -3,10 +3,26 @@ import static org.junit.jupiter.api.Assertions.*;
 class AbsoluteNumberCalculatorTest {
 
     @org.junit.jupiter.api.Test
-    void testFindAbsolute() {
+    void test1FindAbsolute() {
         int number=0;
         int expected=0;
         int result=AbsoluteNumberCalculator.findAbsolute(number);
         assertEquals(result,expected);
     }
+    @org.junit.jupiter.api.Test
+    void test2FindAbsolute() {
+        int number=1;
+        int expected=1;
+        int result=AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(result,expected);
+    }
+    @org.junit.jupiter.api.Test
+    void testFindAbsoluteNegative1() {
+        int number = -1;
+        int expected = 1;
+        int result = AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(expected, result);
+    }
+
+
 }
